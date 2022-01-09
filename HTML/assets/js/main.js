526a1bc2117049afe1c6c761292ba67e25c6ebc7
milanbhanushali <60770889+milanbhanushali@users.noreply.github.com>
2022-01-09 13:35:01 +0530

    Contact , Prices Page
    Completed mobile view , tablet view

--- HTML/assets/js/main.js
+++ HTML/assets/js/main.js
@@ -1,11 +1,28 @@
 const navMenu = document.getElementById('nav-menu'),
 toggleMenu = document.getElementById('toggle-menu'),
-closeMenu = document.getElementById('close-menu')
-
+closeMenu = document.getElementById('close-menu'),
+btnBookNow = document.getElementById('btnBookNow'),
+btnLogin = document.getElementById('btnLogin'),
+btnBecomeHelper = document.getElementById('btnBecomeHelper')
 
 toggleMenu.addEventListener('click',() => {
     navMenu.classList.toggle('custom-show')
+    btnBookNow.classList.remove('custom-bg-green-btn')
+    btnLogin.classList.remove('custom-bg-green-btn')
+    btnBecomeHelper.classList.remove('custom-bg-green-btn')
 })
+
+
 closeMenu.addEventListener('click',() => {
     navMenu.classList.remove('custom-show')
 })
+
+
+var header = document.getElementById('abc')
+
+header.addEventListener('scroll',() =>{
+    header.classList.add('bg-dark')
+    header.classList.remove('bg-transparent')
+})
+
+console.log("hello peter");
